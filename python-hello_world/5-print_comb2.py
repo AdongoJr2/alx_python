de66ff83820989cf7ce2bc91ef01cd03466ae3a6
Moses Adongo <moses.adongo@zeraki.co.ke>
2023-07-18 20:17:57 +0300

    fix: 5. 00...99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,5 @@
 for i in range(100):
-    print("{}".format(str(i).zfill(2)), end="")
     if i == 99:
-      print("")
+      print("{}".format(str(i).zfill(2)))
     else:
-      print(",", end=" ")
+      print("{}".format(str(i).zfill(2)), end=", ")

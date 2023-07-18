78d83a15fbe82cc05687a74f94ebd56f5e756e5d
Moses Adongo <moses.adongo@zeraki.co.ke>
2023-07-18 23:01:59 +0300

    5. Prime Number Function

---
+++ python-functions/5-prime.py
@@ -0,0 +1,21 @@
+def is_prime(number):
+    is_prime_number = False
+
+    if number == 1:
+        return is_prime_number
+    else:
+        if number > 1:
+            for i in range(2, number):
+                if (number % i) == 0:
+                    is_prime_number = True
+                    break
+            
+            if is_prime_number:
+                is_prime_number = False
+            else:
+                is_prime_number = True
+
+    return is_prime_number
+
+
+print(is_prime(20))

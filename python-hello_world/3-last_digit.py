af31eff82c8c7c74c3bc2c7083e61ef4d32011e4
Moses Adongo <moses.adongo@zeraki.co.ke>
2023-07-18 19:31:17 +0300

    fix: 3. The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,7 +5,7 @@ number = random.randint(-10000, 10000)
 last_digit = int(str(number)[-1], base=10)
 
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater that 5".format(number, last_digit))
+    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
     print("Last digit of {} is {} and is zero".format(number, last_digit))
 else:

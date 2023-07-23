13b3cd3ab74f8e8b8e1da34b56bafe3d3fd925d8
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-07-23 16:01:53 +0300

    3. Best score

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,15 @@
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+
+    largest_value = float('-inf')
+    largest_key = None
+
+    for key in a_dictionary:
+        item = a_dictionary[key]
+
+        if item > largest_value:
+            largest_value = item
+            largest_key = key
+
+    return largest_key

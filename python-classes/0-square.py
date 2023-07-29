865b876903c8f7e262782bf760f9926aa60d7537
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-07-29 10:09:56 +0300

    0. Square with size

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+"""Square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initializes the data
+
+        Args:
+            size (Any): The size of the Square
+        """
+        self.__size = size

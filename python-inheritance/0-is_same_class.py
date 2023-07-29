61b9e71a80439b631c761ab0156c755b870cece6
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-07-29 12:51:29 +0300

    fix: 0. Exact same object

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,2 +1,18 @@
+"""My module"""
+
+
 def is_same_class(obj, a_class):
-    return type(obj) == a_class
+    """Checks whether the object is strictly an instance of
+    the class passed
+
+    Args:
+        obj (Any): The object to check
+        a_class (Any): The class to check against
+
+    Returns:
+        bool: The check result
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False

012d8e0fb69d818769b0afd27291a037ec812170
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-07-29 15:02:06 +0300

    8. Square #1

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,15 @@
+"""Square module"""
+Rectangle = __import__('7-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size

1cc95c0535db05ae526c44facb74457f6c10332f
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-06 02:09:00 +0300

    fix: 5. __str__
    - PEP8 validation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -121,6 +121,8 @@ class Rectangle(Base):
         Returns:
             str: [Rectangle] (`id`) `x`/`y` - `width`/`height`
         """
-        return "[Rectangle] ({id}) {x}/{y} - {width}/{height}".format(
+        res = "[Rectangle] ({id}) {x}/{y} - {width}/{height}".format(
             id=self.id, x=self.x, y=self.y, width=self.width, height=self.height
         )
+
+        return res

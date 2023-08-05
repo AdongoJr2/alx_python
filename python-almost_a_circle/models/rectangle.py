6be17a4c5cb6f66a0b73f14eeda0f9a19e5a1ba2
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-06 01:33:58 +0300

    2. Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,12 +7,15 @@ class Rectangle(Base):
     """Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        if id is not None:
+            self.__integer_check(id, "id")
+
         super().__init__(id)
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -20,6 +23,8 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        self.__integer_check(value, "width")
+        self.__gt_0_check(value, "width")
         self.__width = value
 
     @property
@@ -28,6 +33,8 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.__integer_check(value, "height")
+        self.__gt_0_check(value, "height")
         self.__height = value
 
     @property
@@ -36,6 +43,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        self.__integer_check(value, "x")
+        self.__gte_0_check(value, "x")
         self.__x = value
 
     @property
@@ -44,4 +53,18 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        self.__integer_check(value, "y")
+        self.__gte_0_check(value, "y")
         self.__y = value
+
+    def __integer_check(self, value, attribute_name=""):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(attribute_name))
+
+    def __gt_0_check(self, value, attribute_name=""):
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(attribute_name))
+
+    def __gte_0_check(self, value, attribute_name=""):
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(attribute_name))

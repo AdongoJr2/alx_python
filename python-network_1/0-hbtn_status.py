9d02dd7cc74fea658583480667550d2e08e336b0
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-06 14:53:39 +0300

    0. What's my status? #1
    - Added a README.md file

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+"""My module"""
+
+from requests import get
+
+if __name__ == "__main__":
+    response = get("https://alu-intranet.hbtn.io/status")
+    content = str(response.content, "UTF-8")
+
+    formatted_response = """Body response:
+        - type: {}
+        - content: {}""".format(type(content), content)
+
+    print(formatted_response)
+
+    # print("- type: {}".format(type(content)))
+    # print("- content: {}".format(content))

0471004b56a16914170e271416c1b334ab8f19aa
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-06 15:30:32 +0300

    fix: 0. What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,9 +7,11 @@ if __name__ == "__main__":
     response = get("https://alu-intranet.hbtn.io/status")
     content = str(response.content, "UTF-8")
 
-    formatted_response = """Body response:
-        - type: {}
-        - content: {}""".format(type(content), content)
+    formatted_response = """
+Body response:
+    - type: {}
+    - content: {}
+""".strip().format(type(content), content)
 
     print(formatted_response)
 

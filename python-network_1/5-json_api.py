51e92035d712c269007e0aacc79ec5be5b30aca1
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-06 17:21:31 +0300

    4. Search API

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""My module"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = ""
+
+    try:
+        q = sys.argv[1]
+    except IndexError:
+        pass
+
+    response = requests.post(url, {}, params={"q": q})
+
+    try:
+        jsonResponse = response.json()
+        if len(jsonResponse) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(jsonResponse["id"], jsonResponse["name"]))
+    except:
+        print("Not a valid JSON")

bedc9a6628d8b07f9ed4098d7d146d52a42baa77
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-06 17:03:03 +0300

    fix: 3. Error code #1
    - PEP8 validation

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     response = requests.get(url)
-    
+
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
-      print(response.content.decode())
+        print(response.content.decode())

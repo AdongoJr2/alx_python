72f0f0c40fb06c6b89e74a108d73bf539ac5dc5f
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-06 14:27:40 +0300

    10. Square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,3 +20,12 @@ class Square(Rectangle):
             x=self.x, y=self.y,
             size=self.width
         )
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value

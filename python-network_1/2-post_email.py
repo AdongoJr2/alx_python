1796695ee982b841a7261e25506e7851ef47dc9e
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-10 20:51:11 +0300

    Fix: 2. POST an email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    response = requests.post(url, {}, params={"email": email})
+    response = requests.post(url, {"email": email})
     print(response.content.decode())

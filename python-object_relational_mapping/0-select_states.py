41d5d0783dc31681cd238921abcd529b7f775555
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-19 22:58:03 +0300

    0. Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""My module"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = "localhost"
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+
+    sql_query = "SELECT * from states ORDER BY id ASC"
+    cur.execute(sql_query)
+
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
+
+    cur.close()
+    db.close()

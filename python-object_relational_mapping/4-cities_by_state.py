238296b31cb12892b1db23c983bab2319010b4dd
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-19 23:46:11 +0300

    4. Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""My module"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = "localhost"
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+
+    cur.execute(
+        """SELECT * from cities ORDER BY id ASC"""
+    )
+
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
+
+    cur.close()
+    db.close()

3db1fb8a53a673712ac92ce20cdabc217d62bced
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-19 23:36:16 +0300

    3. SQL Injection...

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""My module"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    matches_key = sys.argv[4]
+    host = "localhost"
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * from states WHERE name = BINARY '%s' ORDER BY id ASC",
+        (matches_key,)
+    )
+
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
+
+    cur.close()
+    db.close()

5833e33bc15c1781150faf152730ae480ed480a7
dev.mosesadongo@gmail.com <moses.adongo@zeraki.co.ke>
2023-08-31 21:38:41 +0300

    6. Odd or even?

---
+++ python-web_framework/6-number_odd_or_even.py
@@ -0,0 +1,48 @@
+"""My module"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def dynamic_text(text):
+    response = 'C {}'.format(text.replace('_', ' '))
+    return response
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def dynamic_pyhton_text(text):
+    response = 'Python {}'.format(text.replace('_', ' '))
+    return response
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    response = '{} is a number'.format(n)
+    return response
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
